1892b89e60a6390be7a1c4f6509f6edcb26ff652
CristianDG <39601699+CristianDG@users.noreply.github.com>
2023-04-13 01:36:49 -0300

    add: um esqueleto melhor da api de ocorrencias

--- src/app.py
+++ src/app.py
@@ -1,8 +1,33 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, request
+from controllers import OcorrenciaController
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "teste2"
+
+@app.get("/ocorrencias")
+def listar_ocorrencias():
+    return OcorrenciaController.listar()
+
+@app.post("/ocorrencias")
+def registrar_ocorrencia():
+    ocorrencia = request.get_json()
+
+    if not ocorrencia['descricao']:
+        return {'error': 'descrição inválida'}, 401
+
+    # TODO: falta validar
+    # - [ ] a existencia de todos os campos
+
+    return OcorrenciaController.registrar(ocorrencia)
+
+@app.patch("/ocorrencias/<int:id_ocorrencia>/<int:id_setor>")
+def encaminhar_ocorrencia(id_ocorrencia, id_setor):
+
+    # TODO: falta validar
+    # - [ ] token jwt para gestor
+
+    return OcorrenciaController.encaminhar(id_ocorrencia, id_setor)
--- src/controllers/OcorrenciaController.py
+++ src/controllers/OcorrenciaController.py
@@ -1,13 +1,37 @@
+from tipos import Ocorrencia
+from dataclasses import asdict
 
+OCORRENCIAS: dict[int, Ocorrencia] = dict()
+ID_OCORRENCIA = 0
 
 def listar():
-    assert False, "Not Implemented"
-    pass
+    # FIXME: usar assim até integrar com o banco
+    return [{'id': id, **ocorrencia.dict()} for id, ocorrencia in OCORRENCIAS.items()]
 
-def encaminhar():
-    assert False, "Not Implemented"
-    pass
+def encaminhar(id_ocorrencia: int, id_setor: int):
+    # FIXME: usar assim até integrar com o banco
 
-def registrar():
-    assert False, "Not Implemented"
-    pass
+    # TODO: falta controlar
+    # - [ ] se existe a ocorrencia
+    # - [ ] se existe o setor
+
+    ocorrencia = OCORRENCIAS[id_ocorrencia]
+    print(ocorrencia)
+    ocorrencia.id_setor = id_setor
+    return ocorrencia.dict()
+
+
+def registrar(dados_ocorrencia: dict) -> Ocorrencia:
+    # FIXME: usar assim até integrar com o banco
+    global OCORRENCIAS, ID_OCORRENCIA
+
+    # TODO: falta controlar
+    # - [ ] se existe o local
+    # - [ ] se existe o setor
+
+    ocorrencia = Ocorrencia(**dados_ocorrencia, status='Aberto', data_resolucao=None)
+    OCORRENCIAS[ID_OCORRENCIA] = ocorrencia
+    ID_OCORRENCIA += 1
+
+
+    return { **ocorrencia.dict(), 'id': ID_OCORRENCIA }
