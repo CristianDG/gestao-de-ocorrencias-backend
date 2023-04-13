18ee926dfa4492b26e9dc340353c6f78595d209c
CristianDG <39601699+CristianDG@users.noreply.github.com>
2023-04-13 01:39:18 -0300

    fix: funcionamento usando python 3.9

--- src/tipos.py
+++ src/tipos.py
@@ -1,17 +1,16 @@
-from dataclasses import dataclass, KW_ONLY, asdict
+from dataclasses import dataclass, asdict
 from typing import Union
 import datetime
 
 @dataclass
 class Ocorrencia:
-    status: Union[str, None]
     descricao: str
+    id_local: int
+    id_setor: int
+    status: Union[str, None]
     email_cidadao: Union[str, None]
     nome_cidadao: Union[str, None]
     data_criacao: datetime.datetime = datetime.datetime.now()
     data_resolucao: Union[datetime.datetime, None] = None
-    _: KW_ONLY
-    id_local: int
-    id_setor: int
 
     dict = asdict
