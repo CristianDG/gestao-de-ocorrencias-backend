43ea87e09cefa0f414c8d871504766eb66190ea8
CristianDG <39601699+CristianDG@users.noreply.github.com>
2023-04-23 21:19:52 -0300

    fix: removendo código desnecessario

--- src/app.py
+++ src/app.py
@@ -14,25 +14,12 @@ app = Flask(__name__)
 
 load_dotenv(find_dotenv(".env"))
 
-#configuração das varáveis de ambiente de conexão com o banco DE PRODUÇÃO
-app.config["PROD_DB_HOST"] = os.getenv("PROD_DB_HOST")
-app.config["PROD_DB_NAME"] = os.getenv("PROD_DB_NAME")
-app.config["PROD_DB_PASSWORD"] = os.getenv("PROD_DB_PASSWORD")
-app.config["PROD_DB_PORT"] = os.getenv("PROD_DB_PORT")
-app.config["PROD_DB_USER"] = os.getenv("PROD_DB_USER")
-app.config["CLIENT_ENCODING"] = os.getenv("CLIENT_ENCODING")
-
-#configuração das varáveis de ambiente de conexão com o banco DE AUTENTICAÇÃO
-app.config["AUTH_DB_HOST"] = os.getenv("AUTH_DB_HOST")
-app.config["AUTH_DB_NAME"] = os.getenv("AUTH_DB_NAME")
-app.config["AUTH_DB_PASSWORD"] = os.getenv("AUTH_DB_PASSWORD")
-app.config["AUTH_DB_PORT"] = os.getenv("AUTH_DB_PORT")
-app.config["AUTH_DB_USER"] = os.getenv("AUTH_DB_USER")
 
 
 def formatar_erro(erro):
     if os.getenv('DEBUG'):
         print(erro)
+
     try:
         return ({'error': erro.args[0].value[0]}, erro.args[0].value[1])
     except Exception as e:
